f19029f060a0470e75864b95d6b20a81e78d7fdc
Sandro Motyl <sandro.motyl@gmail.com>
2019-06-10 16:52:37 -0300

    versao atualizada estavel

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -23,18 +23,19 @@ module.exports = {
                 productsJson.push(
                     {
                         "description": productsTableDOM.rows[i].cells[0].getElementsByClassName('txtTit2')[0].textContent,
-                        "total": productsTableDOM.rows[i].cells[1].getElementsByClassName('valor')[0].textContent,
-                        "quantity": productsTableDOM.rows[i].cells[0].getElementsByClassName('Rqtd')[0].textContent.split(":").pop().replace(/(\r\n|\n|\r)/gm, "").trim(),
+                        "total": productsTableDOM.rows[i].cells[1].getElementsByClassName('valor')[0].textContent.replace(",", "."),
+                        "quantity": productsTableDOM.rows[i].cells[0].getElementsByClassName('Rqtd')[0].textContent.split(":").pop().replace(/(\r\n|\n|\r)/gm, "").trim().replace(",", "."),
                         "unity": productsTableDOM.rows[i].cells[0].getElementsByClassName('RUN')[0].textContent.split(":").pop().replace(/(\r\n|\n|\r)/gm, "").trim(),
-                        "unityPrice": productsTableDOM.rows[i].cells[0].getElementsByClassName('RvlUnit')[0].textContent.split(":").pop().replace(/(\r\n|\n|\r)/gm, "").trim(),
+                        "unityPrice": productsTableDOM.rows[i].cells[0].getElementsByClassName('RvlUnit')[0].textContent.split(":").pop().replace(/(\r\n|\n|\r)/gm, "").trim().replace(",", "."),
                     }
                 )
 
             listinhaJson.push(
                 {
+                    "id": nfcInfoDOM.querySelector('li').textContent.split("Emissão: ").pop().split("-").shift().trim().replace(" ", "_"),
                     "date": nfcInfoDOM.querySelector('li').textContent.split("Emissão: ").pop().split(" ").shift().replace(/(\r\n|\n|\r)/gm, "").trim(),
                     "quantity": nfcTotalDOM.getElementsByClassName('totalNumb')[0].textContent,
-                    "total": nfcTotalDOM.getElementsByClassName('totalNumb')[1].textContent,
+                    "total": nfcTotalDOM.getElementsByClassName('totalNumb')[1].textContent.replace(",", "."),
                     "paymentType": nfcTotalDOM.getElementsByClassName('tx')[0].textContent,
                     "products": [ productsJson ],
                 }
